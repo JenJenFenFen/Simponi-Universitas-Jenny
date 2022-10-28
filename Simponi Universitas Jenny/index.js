516829202b061c6550acade2ca406f7edae491d9
Jennyver Seztiani Luxman <fenjenjen@gmail.com>
2022-10-28 22:08:51 +0700

    menampilkan server (masih ada beberapa yang tidak berjalan dengan semestinya

--- Simponi Universitas Jenny/index.js
+++ Simponi Universitas Jenny/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const bodyparser = require('body-parser');
+const path = require('path');
 const db = require('./connection');
 const port = 3000;
 
@@ -10,8 +11,24 @@ app.use(bodyparser.urlencoded ({
 
 app.use(bodyparser.json());
 
-app.get('/user', (req, res) => {
-    res.sendFile('./HTML/index-master.html');
+//css
+app.use("/library", express.static(path.join(__dirname, "node_modules/bootstrap/dist/css")));
+
+//jquery
+app.use("/library", express.static(path.join(__dirname, "node_modules/jquery/dist")));
+
+//bootstrap
+app.use("/library",express.static(path.join(__dirname, "node_modules/bootstrap/dist/js")));
+
+//sidebars
+// app.use("/library", express.static(path.join(__dirname, "node_modules/bootstrap-sidebar/dist/css")));
+// app.use("/library", express.static(path.join(__dirname, "node_modules/bootstrap-sidebar/dist/js")));
+
+//jbvalidator
+app.use("/library", express.static(path.join(__dirname, "node_modules/@emretulek/jbvalidator/dist")));
+
+app.get('/homepage-master', (req, res) => {
+    res.sendFile(path.join(__dirname, '/HTML/index-master.html'));
 });
 
 //membaca server
