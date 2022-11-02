fbf31cbb4e704059a1ac35906cd176930bc09592
vinsen <vinsensius.palka@thamrin.co.id>
2022-11-02 16:51:36 +0700

    selesai membuat table editable untuk input jadwal mata kuliah

--- Simponi Universitas Jenny/index.js
+++ Simponi Universitas Jenny/index.js
@@ -11,26 +11,26 @@ app.use(bodyparser.urlencoded ({
 
 app.use(bodyparser.json());
 
-//koneksi ke file css dan js
+// koneksi ke file css dan js
 app.use(express.static(path.join(__dirname, 'library')));
 app.use(express.static(path.join(__dirname, 'function')));
 
-//HTML master
+// HTML master
 app.get('/homepage-master', (req, res) => {
     res.sendFile(path.join(__dirname, '/HTML/index-master.html'));
 });
 
-//HTML dosen
+// HTML dosen
 app.get('/homepage-lecturer', (req, res) => {
     res.sendFile(path.join(__dirname, '/HTML/index-dosen.html'));
 });
 
-//HTML mahasiswa
+// HTML mahasiswa
 app.get('/homepage-student', (req, res) => {
     res.sendFile(path.join(__dirname, '/HTML/index-mahasiswa.html'));
 });
 
-//membaca port
+// membaca port
 app.listen(port, () => {
     console.log(`Listening port ${port}`);
 });
